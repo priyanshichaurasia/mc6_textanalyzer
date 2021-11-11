d57b4aeea52f554525e649b6f06449b569d4aaa4
671160_PRIYANSHI <priyanshic83@gmail.com>
2021-11-11 12:22:42 +0000

    Saving changes from StackRoute workbench on Thu Nov 11 2021 17:52:42 GMT+0530 (India Standard Time)

--- src/main/java/com/niit/textanalyzer/Node.java
+++ src/main/java/com/niit/textanalyzer/Node.java
@@ -2,8 +2,13 @@ package com.niit.textanalyzer;
 
 public class Node {
     // declare the Word object and the recursive reference 'next' that the Node of the
+    Word word;
+    Node next;
     // linked list holds
 
     // initialize data value
-
+    public Node(Word word){
+        this.word=word;
+        next=null;
+    }
 }
--- src/main/java/com/niit/textanalyzer/Word.java
+++ src/main/java/com/niit/textanalyzer/Word.java
@@ -3,20 +3,35 @@ package com.niit.textanalyzer;
 public class Word {
 
     // The Word class declare the word and the occurance
-
+    private String word;
+    private int occurance;
 
     public Word() {
     }
-// Declare parameterized constructor and initialize values
-   public Word(String word,int occurance)
-   {
 
-   }
-// getters and setters
-
-// Override the toString() method in this format Word={word=xxx,wordOccurance=x}
-    @Override
-    public String toString() {
-        return "";
+    // Declare parameterized constructor and initialize values
+    public Word(String word,int occurance)
+    {
+        this.word=word;
+        this.occurance=occurance;
     }
+    // getters and setters
+    public void setWord(String word){
+        this.word=word;
+    }
+    public String getWord(){
+        return word;
+    }
+    public void setOccurance(int occurance){
+        this.occurance=occurance;
+    }
+    public int getOccurance(){
+        return occurance;
+    }
+
+    // Override the toString() method in this format Word={word=xxx,wordOccurance=x}
+        @Override
+        public String toString() {
+            return ("{words="+getWord() +",wordOccurance="+getOccurance()+"}");
+        }
 }
