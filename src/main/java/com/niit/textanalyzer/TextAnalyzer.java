61d3c3d37979e924bd71f1855505754b68f86a67
671160_PRIYANSHI <priyanshic83@gmail.com>
2021-11-11 16:54:50 +0000

    Saving changes from StackRoute workbench on Thu Nov 11 2021 22:24:51 GMT+0530 (India Standard Time)

--- src/main/java/com/niit/textanalyzer/TextAnalyzer.java
+++ src/main/java/com/niit/textanalyzer/TextAnalyzer.java
@@ -14,6 +14,11 @@ public class TextAnalyzer {
         while(line!=null){
         String[] separator = new String []{"," , " " , "-"};
         String [] output = line.split(separator);
+        linkedList.getWord() = output[0];
+        linkedList.getOccurance() = Integer.parseInt(output[1]);
+        List l1 = new List(linkedList.getWord(),linkedList.getOccurance());
+        linkedList.addNode(l1);
+        i=i++;
 
         return linkedList;
     }
