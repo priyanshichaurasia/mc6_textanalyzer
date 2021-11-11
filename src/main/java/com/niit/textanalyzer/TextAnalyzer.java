edc2cbe55618c9fb83ce40fdef12a8da2494561d
671160_PRIYANSHI <priyanshic83@gmail.com>
2021-11-11 12:42:51 +0000

    Saving changes from StackRoute workbench on Thu Nov 11 2021 18:12:51 GMT+0530 (India Standard Time)

--- src/main/java/com/niit/textanalyzer/TextAnalyzer.java
+++ src/main/java/com/niit/textanalyzer/TextAnalyzer.java
@@ -7,7 +7,13 @@ public class TextAnalyzer {
 // This method reads a file and inserts the file data into the linked list
     public LinkedList readAndParseFile(String filePath) {
 
-        LinkedList linkedList = null;
+        LinkedList linkedList = new LinkedList();
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        String line = br.readLine();
+        int i=0;
+        while(line!=null){
+        String[] separator = new String []{"," , " " , "-"};
+        String [] output = line.split(separator);
 
         return linkedList;
     }
